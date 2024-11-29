1dba167d9e4162bdd09a4dd8ef5463f6a366e6cc
khoivu <khoi.vu@nextchain.kr>
2024-11-29 13:52:21 +0700

    feat: overflow title

--- itsjinhk.js
+++ itsjinhk.js
@@ -1,6 +1,10 @@
-// Quick Input Search Blur
-let addQuickInputBlurEl = false
+let quickInputBlurEl = false
+let breadcrumbItems = []
+let breadcrumbFlowEl = false
+let projectTitleContainerEl = false
+let projectTitleValue = ''
 
+// Quick Input Search Blur
 function addQuickInputBlurStyle(isOpen) {
 	const quickSearchBlurEl = document.querySelector('#quick-search-blur')
 	if (!quickSearchBlurEl) return
@@ -48,17 +52,135 @@ function checkQuickInputWidgetElStyle() {
 }
 
 function addQuickInputBlur() {
-	if (addQuickInputBlurEl) return
+	if (quickInputBlurEl) return
 	const parentElement = document.querySelector('.monaco-scrollable-element')
+	if (!parentElement) return
+
 	const newDiv = document.createElement('div')
 	newDiv.setAttribute('id', 'quick-search-blur')
 	parentElement.appendChild(newDiv)
-	addQuickInputBlurEl = true
+	quickInputBlurEl = true
+}
+
+// Breadcrumb
+
+function handleGetBreadcrumbItem() {
+	const folders = document.querySelectorAll(
+		'.folder.monaco-breadcrumb-item > .monaco-icon-label > .monaco-icon-label-container > .monaco-icon-name-container > .label-name'
+	)
+	const files = document.querySelectorAll(
+		'.file.monaco-breadcrumb-item > .monaco-icon-label > .monaco-icon-label-container > .monaco-icon-name-container > .label-name'
+	)
+	// const outline = document.querySelectorAll(
+	// 	'.outline-element.monaco-breadcrumb-item > .monaco-icon-label > .monaco-icon-label-container > .monaco-icon-name-container > .label-name'
+	// )
+	const foundItems = []
+
+	folders.forEach((item) => {
+		foundItems.push(item.innerText)
+	})
+	files.forEach((item) => {
+		foundItems.push(item.innerText)
+	})
+	// outline.forEach((item) => {
+	// 	foundItems.push(item.innerText)
+	// })
+
+	const isSame = foundItems.every(
+		(item, index) => item === breadcrumbItems[index]
+	)
+
+	if (!isSame) {
+		foundItems.forEach((item) => {
+			breadcrumbItems.push(item)
+		})
+	}
+
+	return {isSame, foundItems}
+}
+
+function addBreadcrumbContainer() {
+	if (breadcrumbFlowEl) return
+	const parentElement = document.querySelector('.editor-container')
+	if (!parentElement) return
+
+	const newDiv = document.createElement('div')
+	newDiv.setAttribute('id', 'breadcrumb-flow')
+	parentElement.appendChild(newDiv)
+
+	breadcrumbFlowEl = true
+}
+
+function addBreadcrumbItems() {
+	const breadcrumbEl = document.querySelector('#breadcrumb-flow')
+	if (!breadcrumbEl) return
+
+	const {isSame, foundItems} = handleGetBreadcrumbItem()
+
+	if (isSame) return
+
+	breadcrumbEl
+		.querySelectorAll('.breadcrumb-item-flow')
+		.forEach((el) => el.remove())
+
+	foundItems.forEach((item, index) => {
+		const newSpan = document.createElement('span')
+		newSpan.setAttribute('class', `breadcrumb-item-flow`)
+		newSpan.innerText = item
+		breadcrumbEl.appendChild(newSpan)
+
+		if (index !== foundItems.length - 1) {
+			const newSplash = document.createElement('span')
+			newSplash.setAttribute('class', `breadcrumb-item-flow`)
+			newSplash.innerText = ' > '
+			breadcrumbEl.appendChild(newSplash)
+		}
+	})
+}
+
+// Project title
+function handleGetProjectTitle() {
+	const projectTitle = document.querySelector(
+		'.composite.title > .title-label > h2'
+	)
+	if (!projectTitle) return
+
+	const projectTitleText = projectTitle.textContent
+
+	return projectTitleText
+}
+
+function addProjectTitleContainer() {
+	if (projectTitleContainerEl) return
+	const projectTitleParentEl = document.querySelector(
+		'.part.sidebar.left.pane-composite-part'
+	)
+	if (!projectTitleParentEl) return
+
+	const newDiv = document.createElement('div')
+	newDiv.setAttribute('id', 'project-title-flow')
+	projectTitleParentEl.appendChild(newDiv)
+
+	projectTitleContainerEl = true
+}
+
+function addProjectTitle() {
+	const projectTitleFlowEl = document.querySelector('#project-title-flow')
+	if (!projectTitleFlowEl) return
+
+	const projectTitleText = handleGetProjectTitle()
+	if (projectTitleText === projectTitleValue) return
+	projectTitleValue = projectTitleText
+	projectTitleFlowEl.innerText = projectTitleText
 }
 
 window.addEventListener('DOMContentLoaded', () => {
 	setInterval(() => {
 		checkQuickInputWidgetElStyle()
 		addQuickInputBlur()
+		addBreadcrumbContainer()
+		addBreadcrumbItems()
+		addProjectTitleContainer()
+		addProjectTitle()
 	}, 100)
 })
